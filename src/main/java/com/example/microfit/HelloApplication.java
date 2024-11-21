ef1a803fc7acd26d39c60c2bd9e22c5071bba43e
Warya015 <abdifathamohamed123@gmail.com>
2024-11-21 15:17:25 +0100

    Structuur aangepast

--- src/main/java/com/example/microfit/HelloApplication.java
+++ src/main/java/com/example/microfit/HelloApplication.java
@@ -4,13 +4,15 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.controlsfx.glyphfont.INamedCharacter;
 
+import javax.xml.namespace.QName;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Login_Pagina1.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
